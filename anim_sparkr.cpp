cbbc9912962e0b03b88464f95d2e845a87699870
posipov34 <34444442+posipov34@users.noreply.github.com>
2020-01-31 10:20:12 +0300

    Update anim_sparkr.cpp
    Добавлена поддержка ESP32

--- anim_sparkr.cpp
+++ anim_sparkr.cpp
@@ -21,7 +21,7 @@ void AnimSparkr_shuffleSeq(byte * seq)
 }
 
 void Anim::animSparkr_SetUp() {
-    Serial1.print(F("(sparkr)"));
+    DebugSerial.print(F("(sparkr)"));
     glowSetUp();
     phase = 0;
     curColor = palette->getPalColor((float)rngb()/256);
